6659d98c2bb8a485a54f13a12c4285ec6ec596b7
SanaHameed2 <sana.faiza2@gmail.com>
2025-06-24 18:06:43 +0500

    first commit

---
+++ Variables-for-Numbers/app.js
@@ -0,0 +1,32 @@
+// Task 1: Show Age in Alert
+document.writeln("<hr>");
+document.writeln("<h2>Task 1: Age Alert</h2>");
+var age = 21;
+alert("I am " + age + " years old");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: green;'>I am " + age + " years old</p>");
+document.writeln("<p style='color: gray;'>— End of Task —</p>");
+document.writeln("<hr>");
+
+// Task 2: Visitor Counter
+document.writeln("<h2>Task 2: Visitor Counter</h2>");
+var visitCount = 14;
+alert("You have visited this site " + visitCount + " times");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: blue;'>You have visited this site " + visitCount + " times</p>");
+document.writeln("<p style='color: gray;'>— End of Task —</p>");
+document.writeln("<hr>");
+
+// Task 3: Birth Year and Data Type
+document.writeln("<h2>Task 3: Birth Year</h2>");
+var birthYear = 2004;
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: purple;'>My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof birthYear + "</p>");
+document.writeln("<p style='color: gray;'>— End of Task —</p>");
+document.writeln("<hr>");
+
+// Task 4: Online Order Info
+document.writeln("<h2>Task 4: Online Order</h2>");
+var visitorName = "Muhammad";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: orange;'>" + visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store</p>");
+document.writeln("<p style='color: gray;'>— End of Task —</p>");
+document.writeln("<hr>");
